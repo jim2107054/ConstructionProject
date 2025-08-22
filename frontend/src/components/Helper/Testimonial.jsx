2f1e01f0eca033b8c9983629aec9ac24e06af492
jim2107054 <mdjahid205hasan@gmail.com>
2025-08-22 20:09:38 +0600

    added testmonials to home page

---
+++ frontend/src/components/Helper/Testimonial.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import CreateCard from './CreateCard';
+
+
+const Testimonial = () => {
+  const cardsData = [
+        {
+            image: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=200',
+            name: 'Briar Martin',
+            handle: '@neilstellar',
+            date: 'April 20, 2025'
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=200',
+            name: 'Avery Johnson',
+            handle: '@averywrites',
+            date: 'May 10, 2025'
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1527980965255-d3b416303d12?w=200&auto=format&fit=crop&q=60',
+            name: 'Jordan Lee',
+            handle: '@jordantalks',
+            date: 'June 5, 2025'
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?w=200&auto=format&fit=crop&q=60',
+            name: 'Avery Johnson',
+            handle: '@averywrites',
+            date: 'May 10, 2025'
+        },
+    ];
+
+
+    return (
+        <>
+            <style>{`
+            @keyframes marqueeScroll {
+                0% { transform: translateX(0%); }
+                100% { transform: translateX(-50%); }
+            }
+
+            .marquee-inner {
+                animation: marqueeScroll 25s linear infinite;
+            }
+
+            .marquee-reverse {
+                animation-direction: reverse;
+            }
+        `}</style>
+
+            <div className="marquee-row w-full mx-auto max-w-5xl overflow-hidden relative">
+                <div className="absolute left-0 top-0 h-full w-20 z-10 pointer-events-none bg-gradient-to-r from-white to-transparent"></div>
+                <div className="marquee-inner flex transform-gpu min-w-[200%] pt-10 pb-5">
+                    {[...cardsData, ...cardsData].map((card, index) => (
+                        <CreateCard key={index} card={card} />
+                    ))}
+                </div>
+                <div className="absolute right-0 top-0 h-full w-20 md:w-40 z-10 pointer-events-none bg-gradient-to-l from-white to-transparent"></div>
+            </div>
+        </>
+    )
+}
+
+export default Testimonial
--- frontend/src/components/frontend/Home.jsx
+++ frontend/src/components/frontend/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Testimonial from '../Helper/Testimonial'
 
 const Home = () => {
   return (
     <div className='mt-16'>
       Home
+      <Testimonial />
     </div>
   )
 }
